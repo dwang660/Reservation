c583e569711925bb31879a3809f91867b715ccf3
Dong Wang <wzmhong@gmail.com>
2021-11-11 17:07:30 -0600

    add user update profile

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Profile
 from django.contrib.auth.forms import UserCreationForm
-from phonenumber_field.modelfields import PhoneNumberField
 
 class UserRegisterForm(UserCreationForm):
 
@@ -10,8 +9,14 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
 
-class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['phone']
--- django_project/users/models.py
+++ django_project/users/models.py
@@ -5,3 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = PhoneNumberField(null=False, blank=False, unique=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
