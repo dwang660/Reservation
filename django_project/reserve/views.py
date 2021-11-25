3caffe7e7dcb4cfdf9f55dd85345e545b400ec0a
victorchkwok <61713215+victorchkwok@users.noreply.github.com>
2021-11-24 22:12:43 -0600

    trial1

--- django_project/reserve/views.py
+++ django_project/reserve/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .forms import PostForm
 # Create your views here.
+def new(request):
+    if request.method == "POST":
+        form = PostForm(request.Post)
+    else:
+        form = PostForm()
+    return render(request, "posts/new.html",{"form":form})
--- django_project/table/models.py
+++ django_project/table/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class reserve(models.Model):
+  reservation_id= models.IntegerField(max_length=10)
+  number_adult= models.IntegerField(max_length=10)
+  number_kids= models.IntegerField(max_length=10)
+  table_id_1= models.IntegerField(max_length=10)
+  table_id_2= models.IntegerField(max_length=10)
+  name= models.CharField(max_length=50)
+  phone= models.IntegerField(max_length=10)
+  reservation_time= models.TimeField()
