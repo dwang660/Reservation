a1deb89cd7621ce3ebc2ba0f511939ecc118b7ef
victorchkwok <61713215+victorchkwok@users.noreply.github.com>
2021-11-27 23:57:50 -0600

    update

--- django_project/reserve/models.py
+++ django_project/reserve/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 class reserves(models.Model):
-  reservation_id= models.IntegerField(max_length=10)
-  number_adult= models.IntegerField(max_length=10)
-  number_kids= models.IntegerField(max_length=10)
-  table_id_1= models.IntegerField(max_length=10)
-  table_id_2= models.IntegerField(max_length=10)
-  name= models.CharField(max_length=50)
-  phone= models.IntegerField(max_length=10)
+  Reservee_Last_name= models.CharField(max_length=50)
+  Reservee_First_name= models.CharField(max_length=50)
+  number_of_adult= models.IntegerField(max_length=10)
+  numbe_of_kids= models.IntegerField(max_length=10)
+  table_id= models.IntegerField(max_length=10)
+  Tele_Num= models.IntegerField(max_length=10)
   reservation_time= models.TimeField()
-  
+  Reservation_ID= models.IntegerField(max_length=10)
+
   def __str__(self):
       return f"(self.title)"
