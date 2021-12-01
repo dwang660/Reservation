5cf9479f273bb058416fc7552a2a7e480a5710f1
Wang Dong <wzmhong@gmail.com>
2021-11-30 19:22:04 -0600

    add reservation and table to admin based on Dunya's code

--- django_project/reserve/admin.py
+++ django_project/reserve/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Reservation
+
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'table_id', 'table2nd_id', 'customer_number')
+admin.site.register(Reservation, ReservationAdmin)
